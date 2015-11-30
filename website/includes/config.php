01f90e8a223acc263053595117b1940262f42109
Tristan van der Linden <tristanvdl@gmail.com>
2015-11-30 11:18:02 +0100

    most recent

--- website/includes/config.php
+++ website/includes/config.php
@@ -1,6 +1,5 @@
 <?php
-
 define('DB_HOST','localhost');
 define('DB_NAME','songs');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','root');
---
+++ website/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+include 'includes/config.php';
+include 'includes/database.php';
+include 'models/dayUnlocker.php';
+include 'models/getSongs.php';
+
+include "views/header.html";
+include "views/day.html";
+
+
+
